042443126827514d8f4687f0bda9e8d8bc2808b3
mjayamaj <majirienaye@gmail.com>
2022-09-12 21:35:29 -0700

    100-print_comb3.c fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
-			{	
+			{
 				putchar(n);
 				putchar(m);
 				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
-				}	
+				}
 			}
 		}
 	}
