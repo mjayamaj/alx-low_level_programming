3b2a925e48c8f952aca078119725585cbf706fb8
mjayamaj <majirienaye@gmail.com>
2022-09-15 10:29:51 -0700

    Tired

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,6 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -23,7 +22,6 @@ int largest_number(int a, int b, int c)
 	else
 	{
 		largest = c;
-	}
-
+	}	
 	return (largest);
 }
