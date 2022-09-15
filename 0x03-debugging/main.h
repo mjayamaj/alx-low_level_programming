9b4ae3ff73f226d8fa74b5abab10aa4db8ab1eaa
mjayamaj <majirienaye@gmail.com>
2022-09-15 04:04:11 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,6 +7,5 @@ void positive_or_negative(int i);
 int largest number(int, int, int);
 void print_remaining days (int, int, int);
 int convert_day(int, int);
-void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN H */
+#endif
