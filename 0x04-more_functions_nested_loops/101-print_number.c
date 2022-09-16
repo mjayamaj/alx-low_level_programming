2cc4075ddc2422b92767abf779fbd99608795181
mjayamaj <majirienaye@gmail.com>
2022-09-16 15:57:31 -0700

    task 12 adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,11 @@
-#imclude "main.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
  * print_number - prints an integer
  * @n: integer to be printed
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
