568a5060b2efbae237cd23309f38419e4f1751d4
mjayamaj <majirienaye@gmail.com>
2023-01-06 10:12:43 -0800

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	// Declare a pointer to a hash table and an iterator
 	hash_table_t *ht;
 	unsigned long int i;
 
-	// Allocate memory for the hash table
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return ('\0');
 
-	// Set the size of the hash table and allocate memory for the array
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return ('\0');
-	// Initialize the array with NULL pointers
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
-	// Return the new hash table
+
 	return (ht);
 }
